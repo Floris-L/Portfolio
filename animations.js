7cdf21aea9b8816a2684236e859f226e18167fb9
Floris-L <lamf0001@hz.nl>
2022-09-08 09:36:06 +0200

    add profile and broken code

--- animations.js
+++ animations.js
@@ -2,16 +2,19 @@ const main = document.querySelector("main");
 const mainWidth = main.clientWidth;
 const mainHeight = main.clientHeight;
 
+const headerText = document.querySelector(".header-text-container");
+console.log(headerText.children[0].children[0].style.display)
+
 
 var tween1 = gsap.to(".b1", {
-    duration: 2, 
+    duration: 0.1, 
     y: mainHeight * 0.5, 
     ease: "none", 
     paused: true
 });
 
 var tween2 = gsap.to(".b2", {
-    duration: 2, 
+    duration: 0.1, 
     x: mainWidth * 0.02,
     y: mainHeight * 0.5, 
     ease: "none", 
@@ -19,7 +22,7 @@ var tween2 = gsap.to(".b2", {
 });
 
 var tween3 = gsap.to(".b3", {
-  duration: 2, 
+  duration: 0.1, 
   x: mainWidth * -0.44,
   y: mainHeight * 0.165, 
 
@@ -28,7 +31,7 @@ var tween3 = gsap.to(".b3", {
 });
 
 var tween4 = gsap.to(".b4", {
-  duration: 2, 
+  duration: 0.1, 
   x: mainWidth * -0.42,
   y: mainHeight * 0.165, 
   ease: "none", 
@@ -36,17 +39,39 @@ var tween4 = gsap.to(".b4", {
 });
 
 var tween5 = gsap.to(".b5", {
-  duration: 2, 
-  y: mainHeight * 0.5, 
+  duration: 0.1, 
+  y: mainHeight * - 0.2,
+  scaleX: 4,
+  scaleY: 2,
+  backgroundColor: "#fff",
   ease: "none", 
   paused: true
 });
 
+var tween6 = gsap.to(".floris-lamfers", {
+  duration: 0.1, 
+  y: mainHeight * - 0.01,
+  x: mainWidth * 0.2,
+  scale: 2,
+  ease: "none", 
+  paused: true
+});
 
+function rearrangeHeaderText () {
+  headerText.children[0].children[0].style.display = "none";
+  document.querySelector(".floris-lamfers").classList = "outlined";
+  document.querySelector(".floris-lamfers").style.fontSize = `${mainWidth * 0.1}px`;
+}
   
-  
-tween1.play();
-tween2.play();
-tween3.play();
-tween4.play();
-tween5.play();
+
+function animateMenu() {
+  tween1.play();
+  tween2.play();
+  tween3.play();
+  tween4.play();
+  tween5.play();
+  tween6.play();
+  rearrangeHeaderText();
+}
+
+animateMenu();
