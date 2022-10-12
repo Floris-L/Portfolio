d89f215078edcf7c55dae0a99cac006624fbed8b
Floris-L <lamf0001@hz.nl>
2022-10-12 15:15:30 +0200

    add aside menu

--- scripts/progress-bar.js
+++ scripts/progress-bar.js
@@ -1,5 +1,20 @@
 const progress = document.querySelector(".completed").innerHTML;
 const progressBar = document.querySelector(".progressbar");
 const inverseProgressPercent = -Math.floor(progress / 60 * 100) + 100; 
+const aside = document.querySelector("aside");
+const collapseButton = document.querySelector(".collapse-aside");
+let collapsed = false;
 
 progressBar.style.background = `linear-gradient(to left, transparent ${inverseProgressPercent}%, var(--main-yellow)  ${inverseProgressPercent}%)`;
+
+aside.onclick = e => {
+    if(collapsed === true) {
+    aside.style.translate = "-190px";
+    collapsed = false;
+    }
+}
+
+collapseButton.onclick = e => {
+    aside.style.translate = "190px";
+    collapsed = true;
+}
